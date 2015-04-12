1bc4bbbffee212e9a89a8a478d518b8e86bbff89
Vinayak Sasidharan V S <vinayak.vvs@gmail.com>
2015-04-13 00:10:02 +0530

    added a test file

---
+++ test.py
@@ -0,0 +1,28 @@
+from mnist_helpers import *
+
+import cv2
+
+# this is the image we want to distort
+test_image = './four.png'
+
+if __name__ == '__main__':
+
+    # read the grayscale image
+    image = cv2.imread(test_image, 0)
+
+    # just call the function elastic_transform function 
+    # with a suitable kernel size, alpha and sigma
+    # as a rule of thumb, if use sigma as a value near 6,
+    # alpha 36-40, kernel size 13-15
+    #
+    # NOTE: the input image SHOULD be of square dimension,
+    # ie no.of rows should be equal to number of cols.
+    
+    image = cv2.resize(image, (30,30))
+
+    # get the transformed image
+    distorted_image = elastic_transform(image, kernel_dim=15,
+                                        alpha=7,
+                                        sigma=37)
+
+    cv2.imwrite('./distortd.png', distorted_image)
